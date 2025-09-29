77874899d4ceecb31af29e0f80c4f455c343fd60
Helmi Pirkola <143197627+hpirkola@users.noreply.github.com>
2025-09-29 16:31:58 +0300

    contact page and project page

--- portfolio/src/app/about/about.tsx
+++ portfolio/src/app/about/about.tsx
@@ -1,22 +1,61 @@
+"use client";
+import { useState } from "react"
 import Image from "next/image";
 
+type Mode = "about" | "skills";
+
 export default function AboutContent() {
+	const [mode, setMode] = useState("about");
+	const handleModeChange = (newMode) => {
+		setMode(newMode);
+	};
 	return <>
-	<div className="flex flex-row items-center m-20 py-5 justify-center translate-y-2">
-		<div className="w-120 text-[#30306B] mr-10">
-			<h1 className="font-bold text-4xl mb-5">About me</h1>
-			<p className="mb-3">Hi, I’m Helmi Pirkola and I’m a programmer based in Helsinki, Finland. I’m currently studying at Hive Helsinki, where my focus is on C and C++ programming. Outside of my studies, I explore other languages and frameworks through personal projects (like this portfolio).</p>
-			<p className="mb-3">Before transitioning into tech, I built a career as a circus artist and teacher. That creative and playful side of me will follow me where ever I go next.</p>
-			<p className="mb-3">I love problem-solving, figuring out how things work, and building projects from the ground up. I’m always curious, always learning, and excited to grow with new technologies. I’m currently looking for my first job on this new career path.</p>
-			<p>Thanks for stopping by! &lt;3</p>
+	<div className="flex flex-row items-start m-20 py-5 justify-center translate-y-2">
+		<div className="w-120 text-[#30306B] py-20 mr-10 text-justify">
+			<div className="flex mb-5">
+				<button onClick={() => handleModeChange("about")} className={`text-4xl mr-4 underline-grow ${mode === "about" ? "font-bold" : ""}`} aria-pressed={mode == "about"}>About me</button>
+				<h1 className="text-4xl"> / </h1>
+				<button onClick={() => handleModeChange("skills")} className={`text-4xl ml-4 underline-grow ${mode === "skills" ? "font-bold" : ""}`} aria-pressed={mode == "skills"}>Skills</button>
+			</div>
+			<div className="min-h-[14rem]">
+				{mode === "about" && (
+				<>
+					<p className="mb-3">Hi, my name is Helmi Pirkola and I'm a programmer based in Helsinki, Finland. I’m currently studying at Hive Helsinki, where my focus is on C and C++ programming. Outside of my studies, I explore other languages and frameworks through personal projects (like this portfolio).</p>
+					<p className="mb-3">Before transitioning into tech, I built a career as a circus artist and teacher. That creative and playful side of me will follow me where ever I go next.</p>
+					<p className="mb-3">I love problem-solving, figuring out how things work, and building projects from the ground up. I’m always curious, always learning, and excited to grow with new technologies. I’m currently looking for my first job on this new career path.</p>
+					<p>Thanks for stopping by! &lt;3</p>
+				</>
+				)}
+				{mode === "skills" && (
+					<div className="flex flex-col">
+						<h2 className="font-bold mt-3">Languages</h2>
+						<p className="font-light">C, C++, Python, JavaScript, TypeScript</p>
+						<h2 className="font-bold mt-3">Web Technologies</h2>
+						<p className="font-light">HTML, Tailwind CSS</p>
+						<h2 className="font-bold mt-3">Frameworks & Libraries</h2>
+						<p className="font-light">React, Next.js</p>
+						<h2 className="font-bold mt-3">Build Tools</h2>
+						<p className="font-light">CMake, Make</p>
+						<h2 className="font-bold mt-3">Version Control & Platforms</h2>
+						<p className="font-light">GitHub</p>
+						<h2 className="font-bold mt-3">Other</h2>
+						<p className="font-light"> Juggling, making world's greatest avocado pasta</p>
+					</div>
+				)}
+			</div>
 		</div>
 		<Image
 			src="/me.jpeg"
 			alt="Portrait of me"
 			width={400}
 			height={600}
-			className="rounded-2xl shadow-md ml-10"
+			className="rounded-4xl shadow-md ml-10"
 		/>
 	</div>
 	</>
 }
+
+/*About me / Skills
+	list languages and frameworks in skills
+
+*/
--- portfolio/src/app/contact/contact.tsx
+++ portfolio/src/app/contact/contact.tsx
@@ -1,7 +1,44 @@
+import { MdArrowOutward } from "react-icons/md";
+
 export default function ContactContent() {
 	return <>
-	<div className="flex h-screen justify-center items-center">
-		<div className="font-bold text-4xl text-[#30306B]">Contact info coming soon...</div>
+	<div className="flex flex-col h-screen py-30 items-center">
+		<div className="w-screen text-left ml-50">
+			<h1 className="font-bold text-4xl mb-5 text-[#30306B]">Contact</h1>
+		</div>
+		<hr className="h-px w-350 my-2 bg-[#30306B] border-0" />
+		<div className="flex justify-between w-screen items-start my-5 text-[#30306B]">
+			<div className="flex items-center text-2xl ml-20">
+				<h2 className="font-bold mr-5 ml-5">Mail</h2>
+				<MdArrowOutward />
+				<a href="mailto:mail@mail.com" className="italic font-extralight">
+					<span className="underline-grow ml-4">pirkolahelmi@gmail.com</span>
+				</a>
+			</div>
+			<div className="flex items-start mr-50">
+				<h2 className="text-2xl font-bold mr-5">Other</h2>
+				<div className="flex flex-col text-2xl">
+					<div className="flex flex-row">
+						<MdArrowOutward className="relative top-1"/>
+						<a href="https://github.com/hpirkola" target="_blank" className="italic font-extralight">
+							<span className="underline-grow ml-4">Github</span>
+						</a>
+					</div>
+					<div className="flex flex-row my-5">
+						<MdArrowOutward className="relative top-1"/>
+						<a href="www.linkedin.com/in/helmipirkola" target="_blank" className="italic font-extralight">
+							<span className="underline-grow ml-4">LinkedIn</span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 	</>
 }
+
+/*
+-linkedin
+-email
+-github
+*/
